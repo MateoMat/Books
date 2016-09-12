8bdb98287c134eec5ff7945b4c9521da4d5473ee
Piotr Synowiec <psynowiec@gmail.com>
2016-09-12 17:56:45 +0200

    add form done but not functional yet

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## REST API exercise at CodersLab Back-End course
+
+![img1](_aux/Screen Shot 2016-09-12 at 17.54.29.png)
--- js/app.js
+++ js/app.js
@@ -1,61 +1,33 @@
-$(function () {
+$(function() {
 
-var BTN_MORE = "Więcej";
-var BTN_CLOSE = "Zamknij";
+    var BTN_MORE = "More";
+    var BTN_CLOSE = "Close";
+    var FORM_ADD_BOOK_TITLE = "Add book to library";
+    var LABEL_AUTHOR = "Author";
+    var LABEL_TITLE = "Title";
+    var LABEL_DESCR = "Description";
 
     class Books {
 
         constructor() {}
 
         addBookToList(book) {
-        //     var newEl =
-        // '<div data-id="' + book.id + '"><h2>'
-        // + book.title + '</h2></h3>'
-        // + book.author + '</h3>'
-        // + '<button>Więcej</button>';
-            //$('books').append(newElement);
-//            console.log(book['title']);
-//            console.log(book.title);
-            
-            
-//       var newEl = '<div class="col-md-6 col-md-offset-3">'
-//                    +'<div class="panel panel-info">'
-//                    +   '<div class="panel-heading">';
-//                    +      '<h3 class="panel-title">' + book.title + '</h3>'
-//                    +   '</div>'
-//                    +   '<div class="panel-body">asdjasldk'
-//                    +   '</div>'
-//                    +   '<div class="panel-footer"> Panel Footer</div>'
-//                    +'</div>'
-//                  +'</div>';
-//                  
-//                  
-           var newEl = '<div class="col-md-6 col-md-offset-3" data-id="' + book.id + '">'
-                       + '<div class="panel panel-info">'
-                          + '<div class="panel-heading">'
-                              + '<h3 class="panel-title">' + book.title +' - ' + book.author +'</h3>'
-                          + '</div>'
-                          + '<div class="panel-body">'
-                          + '</div>'
-                          + '<div class="panel-footer"><button>' + BTN_MORE +'</button>'
-                          + '</div>'
-                       + '</div>'
-                    +'</div>';       
-                  
-           $('books').append(newEl);
+
+            var newEl = '<div class="col-md-6 col-md-offset-3" data-id="' + book.id + '">' +
+                '<div class="panel panel-info">' +
+                '<div class="panel-heading">' +
+                '<h3 class="panel-title">' + book.title + ' - ' + book.author + '</h3>' +
+                '</div>' +
+                '<div class="panel-body">' +
+                '</div>' +
+                '<div class="panel-footer"><button>' + BTN_MORE  + '</button>' +
+                '</div>' +
+                '</div>' +
+                '</div>';
+
+            $('books').append(newEl);
 
         }
-
-/*
- * <div class="panel panel-info">
-                            <div class="panel-heading">
-                                <h3 class="panel-title">Panel title</h3>
-                            </div>
-                            <div class="panel-body">
-                                Panel content
-                            </div>
-                        </div>
- */
 
         createBookList() {
             var $this = this;
@@ -63,7 +35,7 @@ var BTN_CLOSE = "Zamknij";
                 url: 'api/books.php',
                 type: 'GET',
                 dataType: 'json'
-            }).done(function (books) {
+            }).done(function(books) {
                 for (var i = 0; i < books.length; i++) {
                     $this.addBookToList(books[i]);
                 }
@@ -72,28 +44,35 @@ var BTN_CLOSE = "Zamknij";
         }
     }
 
-    function closeAllDetails(){
+    function closeAllDetails() {
+        // first change all button names to MORE
+        
         $('books div.panel-body').hide();
+        $('books button').text(BTN_MORE);
     }
 
 
     var books = new Books();
 
     books.createBookList();
-
+    
+    $('div#add_book_title h3').text(FORM_ADD_BOOK_TITLE);
+    
+    $('label#lbl_add_book_author').text(LABEL_AUTHOR);
+    $('label#lbl_add_book_title').text(LABEL_TITLE);
+    $('label#lbl_add_book_descr').text(LABEL_DESCR);
     
 
-    $('books').on('click', 'button', function () {
-        
+    $('books').on('click', 'button', function() {
+
         var $par = $(this).parent('div').parent('div').parent('div');
         var book_id = $par.data('id');
-        console.log(book_id);
-        closeAllDetails();
-        
-        
-        if($(this).text() == BTN_MORE ){
+        //console.log(book_id);
+        //closeAllDetails();
+
+
+        if ($(this).text() == BTN_MORE) {
             // open and read from DB
-            
             $.ajax({
                 url: 'api/books.php',
                 type: 'GET',
@@ -101,26 +80,22 @@ var BTN_CLOSE = "Zamknij";
                 data: {
                     id: book_id
                 },
-        }).done(function (books) {
+            }).done(function(books) {
                 $par.find('div.panel-body').text(books[0].descr);
                 $par.find('div.panel-body').show();
-        });
-          
+            });
+
             // change button name :)
             $(this).text(BTN_CLOSE);
-        }
-        else if( $(this).text() == BTN_CLOSE){
-            // close 
+        } else if ($(this).text() == BTN_CLOSE) {
+            // close
             $par.find('div.panel-body').hide();
+            // change button name
             $(this).text(BTN_MORE);
         }
         console.log();
-        
-        
-        
-        
-        
-        
+
+
         //var $this = this; // zapobiegnie nadpisaniu przez ajax
 
 
